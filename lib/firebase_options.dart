58325f182416ab5ff9a420aa7ae92697dd95cc9c
Jhontabo <jhonse.tajumbina@gmail.com>
2026-08-15 13:58:47 -0500

    fix: clarify firebase error message for unsupported platforms

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -37,7 +37,7 @@ class DefaultFirebaseOptions {
       case TargetPlatform.linux:
         throw UnsupportedError(
           'DefaultFirebaseOptions have not been configured for linux - '
-          'you can reconfigure this by running the FlutterFire CLI again.',
+          'refer to the supported platforms android and web.',
         );
       default:
         throw UnsupportedError(
